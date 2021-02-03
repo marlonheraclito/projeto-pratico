2928ce11aaaf9057854e62d6c7150b5dabfa8b7c
marlonhack66 <marlonheraclito@gmail.com>
2021-02-03 12:57:55 -0100

    v1

--- app/src/main/java/com/example/projeto_pratico/MainActivity.java
+++ app/src/main/java/com/example/projeto_pratico/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.projeto_pratico;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    FloatingActionButton flAdicionar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flAdicionar = findViewById(R.id.btn_adicionar);
+        recyclerView = findViewById(R.id.recyclerView_id);
+
+
+        flAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, Adicionar.class);
+                startActivity(in);
+                onPause();
+            }
+        });
     }
 }
