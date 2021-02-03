33a0dfe18c8bfee8afde98ab37f772428e16206d
marlonhack66 <marlonheraclito@gmail.com>
2021-02-03 13:05:18 -0100

    v1

--- app/src/main/java/com/example/projeto_pratico/MainActivity.java
+++ app/src/main/java/com/example/projeto_pratico/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.projeto_pratico;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -32,5 +35,26 @@ public class MainActivity extends AppCompatActivity {
                 onPause();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_defi:
+                return true;
+
+            case R.id.menu_fav:
+                Intent in = new Intent(MainActivity.this, Favorito.class);
+                startActivity(in);
+                onPause();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
